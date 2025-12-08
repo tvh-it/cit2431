2d8acf20c50e6171bf1f590b8d93a8d55d08f739
HungKoBiet <choigamemor@gmail.com>
2025-12-08 09:19:55 +0700

    ThayHieuDepTrai

---
+++ hung/src/hung/Hung.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hung;
+
+/**
+ *
+ * @author HƯNG
+ */
+public class Hung {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
