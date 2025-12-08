84199cfa6c3efc10e9b919340f6393cbc573affd
tvh-it <tranvanhieubmt001@gmail.com>
2025-12-08 10:06:29 +0700

    hello

--- hieu/src/hieu/Hieu.java
+++ hieu/src/hieu/Hieu.java
@@ -15,7 +15,7 @@ public class Hieu {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //test
+        System.out.println("Hello world");
     }
     
 }
