0b1022b7bce072bec6c1ef940b3a39b30de11e85
TROS <ebanytros@gmail.com>
2025-12-08 16:08:53 +0700

    hello word

--- YTROS/src/ytros/YTROS.java
+++ YTROS/src/ytros/YTROS.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ytros;
 
-/**
- *
- * @author HT81
- */
 public class YTROS {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("=== Bảng cửu chương " + i + " ===");
+
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+
+            System.out.println();
+        }
     }
-    
 }
